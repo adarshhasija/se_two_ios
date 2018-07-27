52a64af0ce494dde10bb698193e62605c20b9c29
Adarsh Hasija <adarshhasija@adarshs-macbook-pro.local>
2018-07-27 23:39:37 +0530

    Added SpeechViewController

---
+++ CustomVision/SpeechViewController.swift
@@ -0,0 +1,235 @@
+/*
+    Copyright (C) 2016 Apple Inc. All Rights Reserved.
+    See LICENSE.txt for this sample’s licensing information
+    
+    Abstract:
+    The primary view controller. The speach-to-text engine is managed an configured here.
+*/
+
+import UIKit
+import Speech
+
+public class SpeechViewController: UIViewController, SFSpeechRecognizerDelegate {
+    // MARK: Properties
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))!
+    
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    
+    private var recognitionTask: SFSpeechRecognitionTask?
+    
+    private let audioEngine = AVAudioEngine()
+    
+    @IBOutlet var textViewTop : UITextView?
+    
+    @IBOutlet var textViewBottom : UITextView!
+    
+    @IBOutlet var recordButton : UIButton?
+    
+    @IBOutlet weak var recordLabel: UILabel?
+    
+    @IBOutlet weak var timerLabel: UILabel?
+    
+    var seconds = 60
+    var timer = Timer()
+    var isTimerRunning = false
+    
+    // MARK: UIViewController
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Disable the record buttons until authorization has been granted.
+        recordButton?.isEnabled = false
+        
+        textViewTop?.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+        recordLabel?.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+        timerLabel?.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+        
+        //self.textViewTop?.layoutManager.allowsNonContiguousLayout = false //Allows scrolling if text is more than screen real-estate
+        //self.textViewBottom.layoutManager.allowsNonContiguousLayout = false
+    }
+    
+    override public func viewDidAppear(_ animated: Bool) {
+        speechRecognizer.delegate = self
+        
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            /*
+                The callback may not be called on the main thread. Add an
+                operation to the main queue to update the record button's state.
+            */
+            OperationQueue.main.addOperation {
+                switch authStatus {
+                    case .authorized:
+                        self.recordButton?.isEnabled = true
+
+                    case .denied:
+                        self.recordButton?.isEnabled = false
+                        self.recordButton?.setTitle("User denied access to speech recognition", for: .disabled)
+                        self.recordLabel?.text = "User has denied access to speech recognition"
+
+                    case .restricted:
+                        self.recordButton?.isEnabled = false
+                        self.recordButton?.setTitle("Speech recognition restricted on this device", for: .disabled)
+
+                    case .notDetermined:
+                        self.recordButton?.isEnabled = false
+                        self.recordButton?.setTitle("Speech recognition not yet authorized", for: .disabled)
+                        self.recordLabel?.text = "Speech recognition not yet authorized"
+                }
+            }
+        }
+    }
+    
+    private func startRecording() throws {
+
+        // Cancel the previous task if it's running.
+        if let recognitionTask = recognitionTask {
+            recognitionTask.cancel()
+            self.recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(AVAudioSessionCategoryRecord)
+        try audioSession.setMode(AVAudioSessionModeMeasurement)
+        try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        let inputNode = audioEngine.inputNode
+        guard let recognitionRequest = recognitionRequest else { fatalError("Unable to created a SFSpeechAudioBufferRecognitionRequest object") }
+        
+        // Configure request so that results are returned before audio recording is finished
+        recognitionRequest.shouldReportPartialResults = true
+        
+        // A recognition task represents a speech recognition session.
+        // We keep a reference to the task so that it can be cancelled.
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { result, error in
+            var isFinal = false
+            
+            if let result = result {
+                self.textViewTop?.text = result.bestTranscription.formattedString
+                self.textViewBottom.text = result.bestTranscription.formattedString
+                if self.textViewBottom.text.count > 0 {
+                    let location = self.textViewBottom.text.count - 1
+                    let bottom = NSMakeRange(location, 1)
+                    self.textViewTop?.scrollRangeToVisible(bottom)
+                    self.textViewBottom.scrollRangeToVisible(bottom)
+                }
+                isFinal = result.isFinal
+                UIAccessibilityPostNotification(UIAccessibilityAnnouncementNotification,
+                    result.bestTranscription.formattedString) //announce for VoiceOver
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.recordButton?.isEnabled = true
+                self.recordButton?.setTitle("Start Recording", for: [])
+                self.recordLabel?.text = "Tap screen to start recording"
+            }
+        }
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        
+        try audioEngine.start()
+        
+        textViewTop?.font = textViewTop?.font?.withSize(30)
+        textViewTop?.text = "(Go ahead, I'm listening)"
+        textViewBottom.font = textViewBottom.font?.withSize(30)
+        textViewBottom.text = "(Go ahead, I'm listening)"
+        UIAccessibilityPostNotification(UIAccessibilityAnnouncementNotification, // announce
+            "Go ahead, I'm listening");
+    }
+
+    // MARK: SFSpeechRecognizerDelegate
+    
+    public func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            recordButton?.isEnabled = true
+            recordButton?.setTitle("Start Recording", for: [])
+            recordLabel?.text = "Tap screen to start recording"
+            
+        } else {
+            recordButton?.isEnabled = false
+            recordButton?.setTitle("Recognition not available", for: .disabled)
+            recordLabel?.text = "Recognition not available"
+        }
+    }
+    
+    // MARK: Interface Builder actions
+    
+    @IBAction func recordButtonTapped() {
+        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate)) //vibration
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            recordButton?.isEnabled = false
+            recordButton?.setTitle("Stopping", for: .disabled)
+            recordLabel?.text = "Stopping"
+            resetTimer()
+            textViewTop?.font = textViewTop?.font?.withSize(18)
+            textViewTop?.text = ""
+            textViewBottom.font = textViewBottom.font?.withSize(18)
+            textViewBottom.text = ""
+            UIAccessibilityPostNotification(UIAccessibilityAnnouncementNotification, // announce
+                "Recording stopped");
+        } else {
+            try! startRecording()
+            runTimer()
+            recordButton?.setTitle("Stop recording", for: [])
+            recordLabel?.text = "Tap screen to stop recording"
+        }
+    }
+    
+    @objc func updateTimer() {
+        seconds -= 1     //This will decrement(count down)the seconds.
+        timerLabel?.text = timeString(time: TimeInterval(seconds)) //This will update the label.
+        if seconds < 1 {
+            resetTimer()
+            recordButtonTapped() //this should stop the recording
+        }
+        if seconds < 11 {
+            timerLabel?.textColor = UIColor.red
+        }
+    }
+    
+    
+    // MARK: Private Helpers
+    
+    func runTimer() {
+        if !isTimerRunning {
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(SpeechViewController.updateTimer)), userInfo: nil, repeats: true)
+            isTimerRunning = true
+        }
+    }
+    
+    func resetTimer() {
+        timer.invalidate()
+        isTimerRunning = false
+        seconds = 60
+        timerLabel?.textColor = UIColor.black
+        timerLabel?.text = "1:00"
+    }
+    
+    func timeString(time:TimeInterval) -> String {
+        
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        
+        return String(format:"%01i:%02i", minutes, seconds)
+        
+    }
+    
+    
+}
+
