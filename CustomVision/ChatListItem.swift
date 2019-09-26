7e271556c57aa60b7c65d05b4d62bbde31080abc
Adarsh Hasija <hasijaadarsh@gmail.com>
2019-09-26 23:16:39 +0200

    code cleanup for ChatListItem

--- CustomVision/ChatListItem.swift
+++ CustomVision/ChatListItem.swift
@@ -8,9 +8,8 @@
 
 import Foundation
 
-class DummyChatListItem { }
 
-class ChatListItem : DummyChatListItem {
+class ChatListItem {
     
     var text : String
     var time : String
@@ -27,32 +26,13 @@ class ChatListItem : DummyChatListItem {
         dateFormatter.dateFormat = "hh:mm a"
         let date12Hour : String = dateFormatter.string(from: date)
         self.time = time ?? date12Hour
-        super.init()
     }
     
     convenience init(text: String, origin: String) {
         self.init(text: text, time: nil, origin: origin, mode: nil)
-     /*   self.text = text
-        self.origin = origin
-        self.mode = nil
-        
-        let date = Date()
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "hh:mm a"
-        let date12Hour = dateFormatter.string(from: date)
-        self.time = date12Hour  */
     }
     
     convenience init(text: String, origin: String, mode: String?) {
         self.init(text: text, time: nil, origin: origin, mode: mode)
-    /*    self.text = text
-        self.origin = origin
-        self.mode = mode
-        
-        let date = Date()
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "hh:mm a"
-        let date12Hour = dateFormatter.string(from: date)
-        self.time = date12Hour  */
     }
 }
