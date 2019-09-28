029f8257515b09acfb0dd0dbc27a1adb6902e427
Adarsh Hasija <hasijaadarsh@gmail.com>
2019-09-28 17:52:15 +0200

    made Apple Watch app independent + added instruction String

---
+++ Suno-Watch Extension/MorecodeInterfaceController.swift
@@ -0,0 +1,27 @@
+//
+//  MorecodeInterfaceController.swift
+//  Suno-Watch Extension
+//
+//  Created by Adarsh Hasija on 28/09/19.
+//  Copyright © 2019 Adam Behringer. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+
+class MorsecodeInterfaceController : WKInterfaceController {
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+}
