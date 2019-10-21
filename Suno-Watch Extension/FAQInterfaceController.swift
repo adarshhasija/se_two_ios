ac6834479b006c886cbb3532736d42aef93bf0f1
Adarsh Hasija <hasijaadarsh@gmail.com>
2019-10-21 10:22:13 +0530

    cleaned up questions to make them shorter

--- Suno-Watch Extension/FAQInterfaceController.swift
+++ Suno-Watch Extension/FAQInterfaceController.swift
@@ -20,13 +20,13 @@ class FAQInterfaceController: WKInterfaceController {
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        faqArray.append(FAQCell(question: "How do I type in morse code?", answer: "Tap the screen for a dot. The watch will tap your wrist once. Swipe right for a dash. The watch will tap your wrist twice."))
-        faqArray.append(FAQCell(question: "How do I turn morse morse code into an alphabet?", answer: "Swipe up on the screen. If the morse code matches an alphabet character, the alphabet will be displayed and the watch will ping you once on your wrist."))
-        faqArray.append(FAQCell(question: "How do I delete what I have typed?", answer: "Just swipe left. The watch will ping you once if the delete was successful"))
-        faqArray.append(FAQCell(question: "How do I get the watch to say the text typed in morse code?", answer: "After you submit the last alphabet character, swipe up again and the watch will say the text aloud. Ensure the watch volume in high and it is not on Silent Mode in the settings app."))
-        faqArray.append(FAQCell(question: "How does someone reply to the morse code message in normal English?", answer: "Lightly long press on the screen. Then choose dictation or typing to enter a message."))
-        faqArray.append(FAQCell(question: "How can I read the English reply?", answer: "The app will automatically convert the english reply to morse code. Rotate the digital crown downwards to scroll through each character of morse code. The watch will tap you once for dot, twice for dash and once again for end of character. You can also rotate the digital crown upwards to scroll back"))
-        faqArray.append(FAQCell(question: "How do I stop reading and start typing in morse code again?", answer: "Just swipe left once. Then you can start typing again."))
+        faqArray.append(FAQCell(question: "Typing in morse code", answer: "Tap the screen for a dot. The watch will tap your wrist once. Swipe right for a dash. The watch will tap your wrist twice."))
+        faqArray.append(FAQCell(question: "Morse code to alphabet", answer: "Swipe up on the screen. If the morse code matches an alphabet character, the alphabet will be displayed and the watch will ping you once on your wrist."))
+        faqArray.append(FAQCell(question: "Deleting characters", answer: "Just swipe left. The watch will ping you once if the delete was successful"))
+        faqArray.append(FAQCell(question: "Make the watch say text", answer: "After you submit the last alphabet character, swipe up again and the watch will say the text aloud. Ensure the watch volume in high and it is not on Silent Mode in the settings app."))
+        faqArray.append(FAQCell(question: "How to reply", answer: "Lightly long press on the screen. Then choose dictation or typing to enter a message."))
+        faqArray.append(FAQCell(question: "Reading a reply", answer: "The app will automatically convert the english reply to morse code. Rotate the digital crown downwards to scroll through each character of morse code. The watch will tap you once for dot, twice for dash and once again for end of character. You can also rotate the digital crown upwards to scroll back"))
+        faqArray.append(FAQCell(question: "Stop reading and start typing in morse code again", answer: "Just swipe left once. Then you can start typing in morse code again."))
         
         // Configure interface objects here.
         faqTable.setNumberOfRows(faqArray.count, withRowType: "FAQRow")
