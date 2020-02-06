efd2f21b36be265d59b5039bd590f0ee94783864
Adarsh Hasija <hasijaadarsh@gmail.com>
2020-02-06 18:50:08 +0530

    code cleanup for TwoPersonSettings

--- CustomVision/TwoPeopleSettingsViewController.swift
+++ CustomVision/TwoPeopleSettingsViewController.swift
@@ -32,126 +32,29 @@ public class TwoPeopleSettingsViewController : UIViewController {
     var pickerData : [String] = []
     
     @IBOutlet weak var mainStackView: UIStackView!
-    @IBOutlet weak var oneView: UIView!
-    @IBOutlet weak var oneProfileAndSettingsStackView: UIStackView!
-    @IBOutlet weak var oneProfilePicAndNameStackView: UIStackView!
-    @IBOutlet weak var oneProfilePicAndDisabilityIconsStackView: UIStackView!
-    @IBOutlet weak var oneDisabilityIconsStackView: UIStackView!
-    @IBOutlet weak var oneHiImageView: UIImageView!
-    @IBOutlet weak var oneViImageView: UIImageView!
-    @IBOutlet weak var onePersonImageView: UIImageView!
-    @IBOutlet weak var oneNameLabel: UILabel!
-    @IBOutlet weak var oneSettingsStackView: UIStackView!
-    @IBOutlet weak var oneSettingsHiAndExplanationStackView: UIStackView!
-    @IBOutlet weak var oneSettingsHiSwitchStackView: UIStackView!
-    @IBOutlet weak var oneHiLabel: UILabel!
-    @IBOutlet weak var oneHiSwitch: UISwitch!
-    @IBOutlet weak var oneHiExplanationLabel: UILabel!
-    @IBOutlet weak var oneSettingsViAndExplanationStackView: UIStackView!
-    @IBOutlet weak var oneSettingsViSwitchStackVIew: UIStackView!
-    @IBOutlet weak var oneViLabel: UILabel!
-    @IBOutlet weak var oneViSwitch: UISwitch!
-    @IBOutlet weak var oneViExplanationLabel: UILabel!
-    
-    
-    @IBOutlet weak var twoView: UIView!
-    @IBOutlet weak var twoProfileAndSettingsStackView: UIStackView!
-    @IBOutlet weak var twoProfileAndNameStackView: UIStackView!
-    @IBOutlet weak var twoProfilePicAndDisabilityIconsStackVIew: UIStackView!
-    @IBOutlet weak var twoPersonImageVIew: UIImageView!
-    @IBOutlet weak var twoDisabilityIconsStackView: UIStackView!
-    @IBOutlet weak var twoHiImageView: UIImageView!
-    @IBOutlet weak var twoViImageView: UIImageView!
-    @IBOutlet weak var twoNameLabel: UILabel!
-    @IBOutlet weak var twoSettingsStackView: UIStackView!
-    @IBOutlet weak var twoSettingsHiAndExplanationStackView: UIStackView!
-    @IBOutlet weak var twoSettingsHiSwitchStackView: UIStackView!
-    @IBOutlet weak var twoHiLabel: UILabel!
-    @IBOutlet weak var twoHiSwitch: UISwitch!
-    @IBOutlet weak var twoHiExplanationLabel: UILabel!
-    @IBOutlet weak var twoSettingsViAndExplanationStackView: UIStackView!
-    @IBOutlet weak var twoSettingsViSwitchStackVIew: UIStackView!
-    @IBOutlet weak var twoViLabel: UILabel!
-    @IBOutlet weak var twoViSwitch: UISwitch!
-    @IBOutlet weak var twoViExplanationLabel: UILabel!
-    
-    
-    @IBOutlet weak var hostPickerView: UIPickerView!
+    @IBOutlet weak var hostProfilePicAndNameStackView: UIStackView!
+    @IBOutlet weak var hostProfilePicAndDisabilityIconsStackView: UIStackView!
+    @IBOutlet weak var hostDisabilityIconsStackView: UIStackView!
+    @IBOutlet weak var hostHiImageView: UIImageView!
+    @IBOutlet weak var hostViImageView: UIImageView!
+    @IBOutlet weak var hostPersonImageView: UIImageView!
+    @IBOutlet weak var hostNameLabel: UILabel!
     @IBOutlet weak var hostRoleLabel: UILabel!
+    
+    @IBOutlet weak var guestProfileAndNameStackView: UIStackView!
+    @IBOutlet weak var guestProfilePicAndDisabilityIconsStackVIew: UIStackView!
+    @IBOutlet weak var guestPersonImageVIew: UIImageView!
+    @IBOutlet weak var guestDisabilityIconsStackView: UIStackView!
+    @IBOutlet weak var guestHiImageView: UIImageView!
+    @IBOutlet weak var guestViImageView: UIImageView!
+    @IBOutlet weak var guestNameLabel: UILabel!
     @IBOutlet weak var guestRoleLabel: UILabel!
+    
     @IBOutlet weak var errorMessageLabel: UILabel! //only needed when we start with deaf-blind mode
     
+    @IBOutlet weak var hostPickerView: UIPickerView!
     
-    
-    
-    @IBAction func oneHiSwitchValueChanged(_ sender: Any) {
-        if oneHiSwitch.isOn {
-            oneHiExplanationLabel?.text = hiSwitchOnExplanationString
-            oneHiImageView?.alpha = 1
-            twoHiSwitch?.isOn = false
-            twoHiExplanationLabel?.text = hiSwitchOffExplanationString
-            twoHiImageView?.alpha = 0.25
-            
-            self.oneHiImageView?.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
-            UIView.animate(withDuration: 1.0,
-                           delay: 0,
-                           usingSpringWithDamping: 0.2,
-                           initialSpringVelocity: 6.0,
-                           options: .allowUserInteraction,
-                           animations: { [weak self] in
-                            self?.oneHiImageView.transform = .identity
-                },
-                           completion: nil)
-        }
-        else {
-            oneHiImageView?.alpha = 0.25
-            oneHiExplanationLabel?.text = hiSwitchOffExplanationString
-            twoHiSwitch?.isOn = true
-            twoHiExplanationLabel?.text = hiSwitchOnExplanationString
-            twoHiImageView?.alpha = 1
-            
-            self.twoHiImageView?.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
-            UIView.animate(withDuration: 1.0,
-                           delay: 0,
-                           usingSpringWithDamping: 0.2,
-                           initialSpringVelocity: 6.0,
-                           options: .allowUserInteraction,
-                           animations: { [weak self] in
-                            self?.twoHiImageView.transform = .identity
-                },
-                           completion: nil)
-        }
-    }
-    @IBAction func oneViSwitchValueChanged(_ sender: Any) {
-        oneViSwitch?.isOn = false
-        self.oneViExplanationLabel.transform = CGAffineTransform(translationX: 20, y: 0)
-        UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
-            self.oneViExplanationLabel.transform = CGAffineTransform.identity
-        }, completion: nil)
-    }
-    @IBAction func twoHiSwitchValueChanged(_ sender: Any) {
-        twoHiSwitch?.isOn = !twoHiSwitch.isOn //Switch back to the old value
-        
-        //Indicate the it is the switch for Person 1 that should be changed in order to change this. Cannot change this directly
-        self.oneHiSwitch?.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
-        UIView.animate(withDuration: 1.0,
-                       delay: 0,
-                       usingSpringWithDamping: 0.2,
-                       initialSpringVelocity: 6.0,
-                       options: .allowUserInteraction,
-                       animations: { [weak self] in
-                        self?.oneHiSwitch.transform = .identity
-            },
-                       completion: nil)
-    }
-    @IBAction func twoViSwitchValueChanged(_ sender: Any) {
-        twoViSwitch?.isOn = false
-        self.twoViExplanationLabel.transform = CGAffineTransform(translationX: 20, y: 0)
-        UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
-            self.twoViExplanationLabel.transform = CGAffineTransform.identity
-        }, completion: nil)
-    }
-    
+
     
     
     public override func viewDidLoad() {
@@ -162,8 +65,6 @@ public class TwoPeopleSettingsViewController : UIViewController {
         hostPickerView.dataSource = self
         errorMessageLabel?.text = ""
         
-        oneViExplanationLabel?.text = hiViString
-        twoViExplanationLabel?.text = hiViString
         //oneViImageView?.alpha = 0.25
         //twoViImageView?.alpha = 0.25
         
@@ -172,26 +73,16 @@ public class TwoPeopleSettingsViewController : UIViewController {
             || inputUserProfileOption == "_2" {
             //No input = deaf
             // _2 = Deaf
-            //oneHiSwitch?.isOn = true
-            //oneHiExplanationLabel?.text = hiSwitchOnExplanationString
-            oneHiImageView?.alpha = 1
-            //twoHiSwitch?.isOn = false
-            //twoHiExplanationLabel?.text = hiSwitchOffExplanationString
-            twoHiImageView?.alpha = 0.25
-            
+            hostHiImageView?.alpha = 1
+            guestHiImageView?.alpha = 0.25
             hostRoleLabel?.text = HiWillTypeString
             guestRoleLabel?.text = noAilmentsWillTalkString
             hostPickerView?.selectRow(0, inComponent: 0, animated: false)
         }
         else if inputUserProfileOption == "_1" {
             // _1 = normal
-            //oneHiSwitch?.isOn = false
-            //oneHiExplanationLabel?.text = hiSwitchOffExplanationString
-            oneHiImageView?.alpha = 0.25
-            //twoHiSwitch?.isOn = true
-            //twoHiExplanationLabel?.text = hiSwitchOnExplanationString
-            twoHiImageView?.alpha = 1
-            
+            hostHiImageView?.alpha = 0.25
+            guestHiImageView?.alpha = 1
             hostRoleLabel?.text = noAilmentsWillTalkString
             guestRoleLabel?.text = HiWillTypeString
             hostPickerView?.selectRow(1, inComponent: 0, animated: false)
@@ -229,17 +120,17 @@ extension TwoPeopleSettingsViewController : UIPickerViewDataSource {
 extension TwoPeopleSettingsViewController : UIPickerViewDelegate {
     
     public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if row == 0 {
+        if component == 0 && row == 0 {
             hostRoleLabel?.text = HiWillTypeString
             guestRoleLabel?.text = noAilmentsWillTalkString
-            oneHiImageView?.alpha = 1
-            twoHiImageView?.alpha = 0.25
+            hostHiImageView?.alpha = 1
+            guestHiImageView?.alpha = 0.25
         }
-        else if row == 1 {
+        else if component == 0 && row == 1 {
             hostRoleLabel?.text = noAilmentsWillTalkString
             guestRoleLabel?.text = HiWillTypeString
-            oneHiImageView?.alpha = 0.25
-            twoHiImageView?.alpha = 1
+            hostHiImageView?.alpha = 0.25
+            guestHiImageView?.alpha = 1
         }
     }
     
