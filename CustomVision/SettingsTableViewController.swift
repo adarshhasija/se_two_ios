b5520434f5aa92015cbecea93084298bbebf9652
Adarsh Hasija <hasijaadarsh@gmail.com>
2020-05-25 15:15:00 +0530

    minor text changes

--- CustomVision/SettingsTableViewController.swift
+++ CustomVision/SettingsTableViewController.swift
@@ -24,7 +24,7 @@ class SettingsTableViewController : UITableViewController {
     private func setupSectionSendToWatch() {
         if isWatchAppInstalled() {
             isAppInstalledLabel?.text = "Apple Watch app installed"
-            sendToWatchExplanationLabel?.text = "You can use the Apple Watch app to type and read morse code. You can also read morse code on your Apple Watch by transferring the message from your phone to your watch. To do this, simply open the app on your Apple Watch and swipe down"
+            sendToWatchExplanationLabel?.text = "You can use the Apple Watch app to type and read morse code. You can also read morse code on your Apple Watch by transferring the message from your phone to your watch. For example: If you got text from your camera and wish to read it on your watch, you can do so.\n\nTo do this, simply open the app on your Apple Watch and swipe down"
             
             /*   let session = WCSession.default
              if session.isReachable {
@@ -33,7 +33,7 @@ class SettingsTableViewController : UITableViewController {
         }
         else {
             isAppInstalledLabel?.text = "Apple Watch app not installed"
-            sendToWatchExplanationLabel?.text = "The Apple Watch app also allows a deaf-blind person to communicate using morse code. You can also send morse code from your iPhone app to your Watch app and read it there"
+            sendToWatchExplanationLabel?.text = "The Apple Watch app also allows a deaf-blind person to communicate using morse code. You can also send morse code from your iPhone app to your Watch app and read it there\n\nIf you have installed the Watch app, ensure your Watch is switched ON and is close to your phone"
         }
     }
     
